fb12f4a9758e16fdec3a511b930f0947dc5fb17c
georg2014 <g.stahn@campus.tu-berlin.de>
2017-02-12 19:29:48 +0100

    Lasca Bot: splitInput
    --takes eg. "b,b,b,b/b,b,b/b,b,b,b/,,/w,w,w,w/w,w,w/w,w,w,w w" and makes ["b,b,b,b/b,b,b/b,b,b,b/,,/w,w,w,w/w,w,w/w,w,w,w","w"]
    splitInput :: String -> [String]
    splitInput x = [(take 46 x), [last x]]

---
+++ LascaBot.hs
@@ -0,0 +1,60 @@
+--- module (NICHT AENDERN!)
+module LascaBot where
+--- imports (NICHT AENDERN!)
+import Util
+import Data.Char
+import System.Environment
+--- external signatures (NICHT AENDERN!)
+getMove   :: String -> String
+listMoves :: String -> String
+
+-- *==========================================* --
+-- |    HIER BEGINNT EURE IMPLEMENTIERUNG!    | --
+-- *==========================================* --
+
+--- types/structures (TODO)
+
+data Color = White | Black
+
+    --- ... ---
+
+--- logic (TODO)
+getMove   s = "g3-f4" -- Eigene Definition einfügen!
+listMoves s = "[g3-f4,...]" -- Eigene Definition einfügen!
+
+    --- ... ---
+
+--- input (TODO)
+
+--takes eg. "b,b,b,b/b,b,b/b,b,b,b/,,/w,w,w,w/w,w,w/w,w,w,w w" and makes ["b,b,b,b/b,b,b/b,b,b,b/,,/w,w,w,w/w,w,w/w,w,w,w","w"]
+splitInput :: String -> [String]
+splitInput x = [(take 46 x), [last x]]
+
+--parseInput :: [String] -> ...
+--parseInput (board:color:[])   = ... (parseColor color) ...
+--parseInput (board:color:move:[]) = ... (parseColor color) ...
+
+parseColor :: String -> Color
+parseColor "w" = White
+parseColor "b" = Black
+
+toInt :: Char -> Int
+toInt c = ((ord c) - (ord 'a') + 1)
+
+    --- ... ---
+
+--- output (TODO)
+
+colorToString :: Color -> String
+colorToString White = "w"
+colorToString Black = "b"
+
+instance Show Color where
+    show = colorToString
+    
+instance Eq Color where
+    (==) White White = True
+    (==) Black Black = True
+    (==) _ _ = False    
+
+    --- ... ---
